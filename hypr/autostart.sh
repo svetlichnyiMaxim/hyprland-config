e01190823af1c286436b4650df08a77fbdc52e0d
be4dev <antileak4@proton.me>
2023-09-06 07:37:11 +0300

    upload

---
+++ hypr/autostart.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
+hyprpaper --config ~/.config/hypr/hyprpaper.conf & 
+waybar --config ~/.config/waybar/config --style ~/.config/waybar/style.css &
+wl-clipboard-history -t &
+wl-paste --watch cliphist store &
+dunst &
+exec-once=/usr/lib/polkit-kde-authentication-agent-1 &
+set GTK_THEME Sweet-Dark
+
